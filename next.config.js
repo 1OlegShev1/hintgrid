4b0727a8dc9b9753df18268fdc0f94beb20fe26d
Oleg Shevchenko <1olegshev@gmail.com>
2026-02-05 17:10:38 +0100

    next config update to prevent blinks on room page access

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   // Only use static export for production build (not in dev)
   ...(process.env.NODE_ENV === 'production' ? { output: 'export' } : {}),
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
